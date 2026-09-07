13ea5929beed894c6003c24c22adcb5356435678
nagcas <studio.nagcas@outlook.it>
2026-09-07 23:16:55 +0200

    feat: Update Contact and User serializers to use specific fields and initialize form state in Contacts component

--- contact/serializer.py
+++ contact/serializer.py
@@ -2,15 +2,16 @@ from rest_framework import serializers
 from .models import Contact
 from django.contrib.auth.models import User
 
+
 class ContactSerializer(serializers.ModelSerializer):
-  
-  class Meta:
-    model = Contact
-    #fields = ('id', 'name', 'last_name', 'content', 'answer', 'done')
-    fields = '__all__'
+
+    class Meta:
+        model = Contact
+        fields = ('id', 'name', 'last_name', 'content', 'answer', 'done')
+
 
 class UserSerializer(serializers.ModelSerializer):
-  
-  class Meta:
-    model = User
-    fields =('id', 'username', 'email', 'password')
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')

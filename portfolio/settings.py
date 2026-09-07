e64d3ddb90ae9700b035bd1b05e1556e08f1e897
nagcas <studio.nagcas@outlook.it>
2026-09-07 23:39:36 +0200

    fix: Update ALLOWED_HOSTS to use environment variable without default value

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = os.environ.get("DEBUG", "False") == "True"
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition

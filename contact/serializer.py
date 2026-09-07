7a85420757826deef8830cbac0dc62f0700691e5
nagcas <studio.nagcas@outlook.it>
2026-09-08 01:12:51 +0200

    feat: Add UserSerializer with password handling and user creation logic

--- contact/serializer.py
+++ contact/serializer.py
@@ -4,14 +4,26 @@ from django.contrib.auth.models import User
 
 
 class ContactSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Contact
         fields = ('id', 'name', 'last_name', 'content')
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True
+    )
 
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+
+        return user

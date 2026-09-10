8c5ab67a707b6129767ec22a76efd4bcf2563b1b
nagcas <studio.nagcas@outlook.it>
2026-09-11 00:29:27 +0200

    feat: Add Vercel Analytics integration and update package dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 import Page404 from "./pages/page404/Page404";
 import Home from "./pages/home/Home";
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
+      {/* Vercel Analytics */}
+      <Analytics />
     </Router>
   );
 }
